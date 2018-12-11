ef0304c52924301a0746462ec20e3b512f49e7e8
s-jooch <s-jooch@49S50013225.it.bsd405.org>
2018-12-11 01:51:13 -0800

    checkpoint 3

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,144 +1,260 @@
-import java.util.*;
+
+
+import java.util.Scanner;
+
+//Algorithm
+// First Rule! Set all Fractions to Improper Fractions to make operations easier
+// produceAnswer = parses operands
+// parseFractions = parses operands string then sets static class fields
+// Each Operation Function or Method performs proper calculation then calls SetResult method
+// SetResult = converts the result to the proper string output. This method also calls GCD and ConvertFromImproperToMixed methods
+// findGcd = used to simplify improper fractions
+// turnImproperFractionToMixedFraction = convert improper fraction (as appropriate) to final results
+
 public class FracCalc {
 
-    /**
-     * Prompts user for input, passes that input to produceAnswer, then outputs the result.
-     * @param args - unused
-     */
-    public static void main(String[] args) 
-    {
-		String q = " ";
-		while (!q.equals("quit")) {
-			Scanner console = new Scanner(System.in);
-			System.out.println("enter a fraction equation. enter 'quit' to end. ");
-			q = console.nextLine();
-			System.out.println(produceAnswer(q));
+	// Operand 1
+	public static int operand1Wholenumber = 0;
+	public static int operand1Numerator = 0;
+	public static int operand1Denominator = 0;
+	public static int operand1ImproperNumerator = 0;
+
+	// Operand 2
+	public static int operand2Wholenumber = 0;
+	public static int operand2Numerator = 0;
+	public static int operand2Denominator = 0;
+	public static int operand2ImproperNumerator = 0;
+
+	// Operator
+	public static String operator = "";
+	public static String finalresult = "";
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.print("Please enter your equation (quit to quit): ");
+		String equation = console.nextLine();
+
+		while (!equation.toLowerCase().equals("quit")) {
+			String results = produceAnswer(equation);
+			System.out.println(results);
+			System.out.print("Please enter your equation (quit to quit): ");
+			equation = console.nextLine();
 		}
+		console.close();
+		System.out.print("Fraction Calculator is Closed!");
+	}
 
-        // TODO: Read the input from the user and call produceAnswer with an equation
-        // Checkpoint 1: Create a Scanner, read one line of input, pass that input to produceAnswer, print the result.
-        // Checkpoint 2: Accept user input multiple times.
-    
-    }
-    
-    /**
-     * produceAnswer - This function takes a String 'input' and produces the result.
-     * @param input - A fraction string that needs to be evaluated.  For your program, this will be the user input.
-     *      Example: input ==> "1/2 + 3/4"
-     * @return the result of the fraction after it has been calculated.
-     *      Example: return ==> "1_1/4"
-     */
-    
-    public static String produceAnswer(String input) {
-    	
-		String[] holder = input.split(" ");
-		String operand1 = holder[0];
-		String operation = holder[1];
-		String operand2 = holder[2];
-		
-        // TODO: Implement this function to produce the solution to the input
-        // Checkpoint 1: Return the second operand.  Example "4/5 * 1_2/4" returns "1_2/4".
-        // Checkpoint 2: Return the second operand as a string representing each part.
-        //               Example "4/5 * 1_2/4" returns "whole:1 numerator:2 denominator:4".
-        // Checkpoint 3: Evaluate the formula and return the result as a fraction.
-        //               Example "4/5 * 1_2/4" returns "6/5".
-        //               Note: Answer does not need to be reduced, but it must be correct.
-        // Final project: All answers must be reduced.
-        //               Example "4/5 * 1_2/4" returns "1_1/5".
-        
-		String whole2 = whole(operand2);
-		String num2 = num(operand2);
-		String denom2 = denom(operand2);
-		
-		String chk2 = "whole:" + whole2 + " numerator:" + num2 + " denominator:"  + denom2;
-		
-		return chk2;
+	public static String produceAnswer(String input) {
+		String[] arrayinput = input.split(" ");
+		if (arrayinput.length < 3) {
+			return "ERROR: Input is in an invalid format.";
+		} else {
+			CalculateEpxression(arrayinput[1], arrayinput[0], arrayinput[2]);
+			for (int i = 3; i < arrayinput.length; i += 2) {
+				CalculateEpxression(arrayinput[i], FracCalc.finalresult, arrayinput[i + 1]);
+			}
+			return FracCalc.finalresult;
+		}
+	}
 
-    }
+	public static void CalculateEpxression(String operator, String operand1, String operand2) {
+		FracCalc.parseFraction(operand1, true);
+		FracCalc.parseFraction(operand2, false);
+		FracCalc.operator = operator;
+		if (FracCalc.operand1Denominator == 0 || FracCalc.operand2Denominator == 0) {
+			FracCalc.finalresult = "ERROR: Cannot divide by zero.";
+		} else {
+			if (operator.equalsIgnoreCase("+")) {
+				FracCalc.Add();
+			} else if (operator.equalsIgnoreCase("*")) {
+				FracCalc.Multiply();
+			} else if (operator.equalsIgnoreCase("-")) {
+				FracCalc.Subract();
+			} else if (operator.equalsIgnoreCase("/")) {
+				FracCalc.Divide();
+			} else if (operator.length() > 1) {
+				FracCalc.finalresult = "ERROR: Input is in an invalid format.";
+			}
+		}
+	}
 
-    // TODO: Fill in the space below with helper methods
-    
-    // find the whole number
-    public static String whole(String a) {
-    	
-    	// mixed num
-    	if(a.contains("_")) {
-    		return a.substring(0, a.indexOf('_'));
-		
-		// just fraction
-    	} else if (a.contains("/")) {
-    		return "0";
-		
-		// just whole
-    	} else {
-    		return a;
-    	}
-    }
-    
-    // find the numerator
-    public static String num(String a) {
-    	
-    	// mixed num
-    	if(a.contains("_")) {
-    		return a.substring(a.indexOf('_') + 1, a.indexOf('/'));
-    		
-		// just fraction
-    	} else if (a.contains("/")) {
-    		return a.substring(0, a.indexOf('/')) ;
-		
-		// just whole
-    	} else {
-    		return "0";
-    	}
-    }
-    
-    // find the denominator
-    public static String denom(String a) {
+	public static void parseFraction(String operand, Boolean operand1) {
+		String fraction = operand;
+		String wholenumber = "0";
+		String numerator = "0";
+		String denominator = "0";
+		String[] splitwholenumber = fraction.split("_");
+		if (splitwholenumber.length == 2) {
+			// Mixed Fraction Condition
+			wholenumber = fraction.split("_")[0];
+			numerator = fraction.split("_")[1].split("/")[0];
+			denominator = fraction.split("_")[1].split("/")[1];
+		} else {
+			// Check if it's whole number only
+			String[] splitnumeratoranddenominator = fraction.split("/");
+			// If there are 2 values in array, this means it's a fraction else it's only a
+			// whole number
+			if (splitnumeratoranddenominator.length == 2) {
+				numerator = splitnumeratoranddenominator[0];
+				denominator = splitnumeratoranddenominator[1];
+			} else {
+				wholenumber = operand;
+				denominator = "1";
+			}
+		}
+		// Set Static Variables
+		SetStaticFieldValues(operand1, Integer.parseInt(wholenumber), Integer.parseInt(numerator),
+				Integer.parseInt(denominator));
+	}
 
-		// fraction or mixed num
-    	if (a.contains("/")) {
-    		return a.substring(a.indexOf('/') + 1) ;
-		
-		// just whole
-    	} else {
-    		return "1";
-    	}
-    }
+	public static void SetStaticFieldValues(Boolean operand1, int wholenumber, int numerator, int denominator) {
+		if (operand1) {
+			FracCalc.operand1Wholenumber = wholenumber;
+			FracCalc.operand1Numerator = numerator;
+			FracCalc.operand1Denominator = denominator;
+			if (wholenumber != 0) {
+				// Check if it's negative then do absolute value and add the negative value back
+				if (Integer.toString(wholenumber).contains("-")) {
+					wholenumber = Integer.parseInt(Integer.toString(wholenumber).split("-")[1]);
+					// Convert back to negative
+					FracCalc.operand1ImproperNumerator = ((wholenumber * denominator) + numerator) * (-1);
+				} else {
+					FracCalc.operand1ImproperNumerator = (wholenumber * denominator) + numerator;
+				}
+			} else {
+				FracCalc.operand1ImproperNumerator = numerator;
+			}
 
-    /**
-     * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
-     *      Use this helper method in the Final Checkpoint to reduce fractions.
-     *      Note: There is a different (recursive) implementation in BJP Chapter 12.
-     * @param a - First integer.
-     * @param b - Second integer.
-     * @return The GCD.
-     */
+		} else {
+			FracCalc.operand2Wholenumber = wholenumber;
+			FracCalc.operand2Numerator = numerator;
+			FracCalc.operand2Denominator = denominator;
+			if (wholenumber != 0) {
+				// Check if it's negative then do absolute value and add the negative value back
+				if (Integer.toString(wholenumber).contains("-")) {
+					wholenumber = Integer.parseInt(Integer.toString(wholenumber).split("-")[1]);
+					// Convert back to negative
+					FracCalc.operand2ImproperNumerator = ((wholenumber * denominator) + numerator) * (-1);
+				} else {
+					FracCalc.operand2ImproperNumerator = (wholenumber * denominator) + numerator;
+				}
+			} else {
+				FracCalc.operand2ImproperNumerator = numerator;
+			}
+		}
+	}
 
-    public static int greatestCommonDivisor(int a, int b)
-    {
-        a = Math.abs(a);
-        b = Math.abs(b);
-        int max = Math.max(a, b);
-        int min = Math.min(a, b);
-        while (min != 0) {
-            int tmp = min;
-            min = max % min;
-            max = tmp;
-        }
-        return max;
-    }
-    
-    /**
-     * leastCommonMultiple - Find the smallest integer that can be evenly divided by two integers.
-     *      Use this helper method in Checkpoint 3 to evaluate expressions.
-     * @param a - First integer.
-     * @param b - Second integer.
-     * @return The LCM.
-     */
-    
-    public static int leastCommonMultiple(int a, int b)
-    {
-        int gcd = greatestCommonDivisor(a, b);
-        return (a*b)/gcd;
-    }
+	public static void SetResult(int numerator, int denominator) {
+		if (numerator % denominator == 0) {
+			FracCalc.finalresult = Integer.toString(numerator / denominator);
+		} else {
+			// Simplify Fractions
+			int gcd = FracCalc.findGcd(numerator, denominator);
+			// Remove negative number on gcd (Get Absolute value)
+			if (Integer.toString(gcd).contains("-")) {
+				gcd = Integer.parseInt(Integer.toString(gcd).split("-")[1]);
+			}
+			int simplifiednumerator = numerator / gcd;
+			int simplifieddenominator = denominator / gcd;
+			// Convert back to Mixed Form
+			FracCalc.finalresult = turnImproperFractionToMixedFraction(simplifiednumerator, simplifieddenominator);
+		}
+	}
+
+	public static int findGcd(int number1, int number2) {
+		if (number2 == 0) {
+			return number1;
+		}
+		return findGcd(number2, number1 % number2);
+	}
+
+	public static String turnImproperFractionToMixedFraction(int numerator, int denominator) {
+		Integer wholenumber = numerator / denominator;
+		Integer remainder = numerator % denominator;
+		// Check to see if the result is a negative result and remove the "-" char from
+		// the fraction and only show it in the whole number
+		if (wholenumber < 0)
+			// if (Integer.toString(remainder).contains("-"))
+		{
+			// Again Check to see if the remainder is negative
+			if (Integer.toString(remainder).contains("-")) {
+				remainder = Integer.parseInt(Integer.toString(remainder).split("-")[1]);
+			}
+		}
+		return wholenumber != 0 ? (wholenumber + "_" + remainder + "/" + denominator) : (remainder + "/" + denominator);
+	}
+
+	// Calculations
+	public static String Add() {
+		int tempnumerator = 0;
+		int commondenominator = 0;
+		if (FracCalc.operand1Denominator == FracCalc.operand2Denominator) {
+			// Call Calculate Numerator based on operator
+			tempnumerator = FracCalc.operand1ImproperNumerator + FracCalc.operand2ImproperNumerator;
+			// Since the denominator are the same let's just use Operand1 denom
+			SetResult(tempnumerator, FracCalc.operand1Denominator);
+		} else {
+			commondenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
+			FracCalc.operand1ImproperNumerator = FracCalc.operand1ImproperNumerator
+					* (commondenominator / FracCalc.operand1Denominator);
+			FracCalc.operand2ImproperNumerator = FracCalc.operand2ImproperNumerator
+					* (commondenominator / FracCalc.operand2Denominator);
+			tempnumerator = FracCalc.operand1ImproperNumerator + FracCalc.operand2ImproperNumerator;
+			SetResult(tempnumerator, commondenominator);
+		}
+		return FracCalc.finalresult;
+	}
+
+	public static String Multiply() {
+		int tempnumerator = 0;
+		int tempdenominator = 0;
+		tempnumerator = FracCalc.operand1ImproperNumerator * FracCalc.operand2ImproperNumerator;
+		tempdenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
+		SetResult(tempnumerator, tempdenominator);
+		return FracCalc.finalresult;
+	}
+
+	public static String Subract() {
+		int tempnumerator = 0;
+		int commondenominator = 0;
+		if (FracCalc.operand1Denominator == FracCalc.operand2Denominator) {
+			// Call Calculate Numerator based on operator
+			tempnumerator = FracCalc.operand1ImproperNumerator - FracCalc.operand2ImproperNumerator;
+			// Since the denominator are the same let's just use Operand1 denom
+			SetResult(tempnumerator, FracCalc.operand1Denominator);
+		} else {
+			commondenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
+			FracCalc.operand1ImproperNumerator = FracCalc.operand1ImproperNumerator
+					* (commondenominator / FracCalc.operand1Denominator);
+			FracCalc.operand2ImproperNumerator = FracCalc.operand2ImproperNumerator
+					* (commondenominator / FracCalc.operand2Denominator);
+			tempnumerator = FracCalc.operand1ImproperNumerator - FracCalc.operand2ImproperNumerator;
+			SetResult(tempnumerator, commondenominator);
+		}
+		return FracCalc.finalresult;
+	}
+
+	public static String Divide() {
+		int tempnumerator = 0;
+		int tempdenominator = 0;
+		// Invert reciprocal of Operand 2
+		// Again Check to see if the remainder is negative
+		if (FracCalc.operand2ImproperNumerator < 0) {
+			FracCalc.operand2ImproperNumerator = Integer
+					.parseInt(Integer.toString(FracCalc.operand2ImproperNumerator).split("-")[1]);
+			int tempvalue = FracCalc.operand2Denominator;
+			FracCalc.operand2Denominator = FracCalc.operand2ImproperNumerator;
+			FracCalc.operand2ImproperNumerator = tempvalue;
+			tempnumerator = (FracCalc.operand1ImproperNumerator * -1) * FracCalc.operand2ImproperNumerator;
+			tempdenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
+		} else {
+			int tempvalue = FracCalc.operand2Denominator;
+			FracCalc.operand2Denominator = FracCalc.operand2ImproperNumerator;
+			FracCalc.operand2ImproperNumerator = tempvalue;
+			tempnumerator = FracCalc.operand1ImproperNumerator * FracCalc.operand2ImproperNumerator;
+			tempdenominator = FracCalc.operand1Denominator * FracCalc.operand2Denominator;
+		}
+		SetResult(tempnumerator, tempdenominator);
+		return FracCalc.finalresult;
+	}
 }
