90929e81e61b125e9d359d2bfd2011e8e11302f8
s-jooch <s-jooch@49S50013225.it.bsd405.org>
2018-12-11 02:06:16 -0800

    lkjlkj

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -157,6 +157,43 @@ public class FracCalc {
 		return findGcd(number2, number1 % number2);
 	}
 
+	 /**
+     * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
+     *      Use this helper method in the Final Checkpoint to reduce fractions.
+     *      Note: There is a different (recursive) implementation in BJP Chapter 12.
+     * @param a - First integer.
+     * @param b - Second integer.
+     * @return The GCD.
+     */
+
+    public static int greatestCommonDivisor(int a, int b)
+    {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+        while (min != 0) {
+            int tmp = min;
+            min = max % min;
+            max = tmp;
+        }
+        return max;
+    }
+    
+    /**
+     * leastCommonMultiple - Find the smallest integer that can be evenly divided by two integers.
+     *      Use this helper method in Checkpoint 3 to evaluate expressions.
+     * @param a - First integer.
+     * @param b - Second integer.
+     * @return The LCM.
+     */
+    
+    public static int leastCommonMultiple(int a, int b)
+    {
+        int gcd = greatestCommonDivisor(a, b);
+        return (a*b)/gcd;
+    }
+}
 	public static String turnImproperFractionToMixedFraction(int numerator, int denominator) {
 		Integer wholenumber = numerator / denominator;
 		Integer remainder = numerator % denominator;
